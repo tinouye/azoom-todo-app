7785395f0a4b0b41a7c88bab9b254980ab3c3d0f
Tomi Inouye <tinouye14@gmail.com>
2024-09-12 23:52:19 -0500

    Fixed build command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   css: [
     '@/global.css'
   ],
+  app: {
+    baseURL: '/', // This is usually '/', unless you're serving from a subdirectory
+    cdnURL: '', // Leave blank unless you're using a CDN
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt']
